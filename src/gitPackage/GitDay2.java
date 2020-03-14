a692b3657714bd163c319ab4f45363f2025050e9
abdurrahim <abdurrahim.ozturk93@gmail.com>
2020-03-14 21:07:31 +0100

    commit 6 added

--- src/gitPackage/GitDay2.java
+++ src/gitPackage/GitDay2.java
@@ -5,6 +5,8 @@ public class GitDay2 {
 	public static void main(String[] args) {
 		System.out.println("Hello world!");
 		System.out.println("hello Git Users!");
+		System.out.println("check if your local is ahead of origin master");
+		
 
 	}
 
