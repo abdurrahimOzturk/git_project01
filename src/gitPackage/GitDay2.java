b24b9f42471003fdeeb29eafa4ed828a444ef0ac
abdurrahim <abdurrahim.ozturk93@gmail.com>
2020-03-14 21:13:31 +0100

    commit 7

--- src/gitPackage/GitDay2.java
+++ src/gitPackage/GitDay2.java
@@ -6,7 +6,7 @@ public class GitDay2 {
 		System.out.println("Hello world!");
 		System.out.println("hello Git Users!");
 		System.out.println("check if your local is ahead of origin master");
-		
+		System.out.println("commit 7");
 
 	}
 
